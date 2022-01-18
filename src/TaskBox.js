f5ceed9cd79c0ce725d7dc0d5ec6ca2d79412b2f
Martin Lewis <martin.lewis4321@gmail.com>
2022-01-18 12:19:26 +0000

    Cosmetic Changes

--- src/TaskBox.js
+++ src/TaskBox.js
@@ -50,9 +50,9 @@ class TaskBox extends React.Component {
               <input name="name" type="text" value={this.state.value1} onChange={this.handleChange}/>
               <br />Duration:
               <DistributionBox id="1" callback={this.distCallback}/>
-              <br />Cost:
+              Cost:
               <DistributionBox id="2" callback={this.distCallback}/>
-              <br />Priority: 
+              Priority: 
               <select name= "priority" value={this.state.value5} onChange={this.handleChange}>
                 <option value = "2">Highest</option>
                 <option value = "1">High</option>
